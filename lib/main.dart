ff0fc07130dd8dcff760417695a3ad70d006b8c4
krisnachy <cahyayuana@gmail.com>
2022-06-27 13:20:55 +0700

    text 1st commit

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,12 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text("Aplikasi Hello World"),
         ),
-        body: const Center(child: Text("Hello World")),
+        body: Center(
+            child: Container(
+                color: Colors.lightBlue,
+                width: 150,
+                height: 50,
+                child: const Text("Hello World"))),
       ),
     );
   }
