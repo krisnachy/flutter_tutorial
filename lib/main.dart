45e2133d6147f6851766da61be47fd0d562d32a9
krisnachy <cahyayuana@gmail.com>
2022-06-27 13:14:13 +0700

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     //Material app adalah widget yg berisi data-data yg diperlukan oleh app yg menggunakan material design
     return MaterialApp(
       //tampilan awal yang akan ditampilkan oleh material app
       home: Scaffold(
-        
+        appBar: AppBar(
+          title: const Text("Aplikasi Hello World"),
+        ),
+        body: const Center(child: Text("Hello World")),
       ),
     );
   }
