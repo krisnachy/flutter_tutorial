65e1c5dbd53d9c3dcc8e73723c8e5f142c2cb338
krisnachy <cahyayuana@gmail.com>
2022-06-27 13:06:44 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //Material app adalah widget yg berisi data-data yg diperlukan oleh app yg menggunakan material design
+    return MaterialApp(
+      //tampilan awal yang akan ditampilkan oleh material app
+      home: Scaffold(
+        
+      ),
+    );
+  }
+}
