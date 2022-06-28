4a9cea3c89425819b27c6781e4171e1f415c7e71
krisnachy <cahyayuana@gmail.com>
2022-06-28 14:08:36 +0700

    anonymous

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,47 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String message = "Ini adala text";
+
+  //anonymous method = method yang tidak punya nama
+  // void tombolDiTekan() {
+  //   setState(() {
+  //     message = "Tombol sudah ditekan";
+  //   });
+  // }
+
+  @override
   Widget build(BuildContext context) {
-    //Material app adalah widget yg berisi data-data yg diperlukan oleh app yg menggunakan material design
     return MaterialApp(
-      //tampilan awal yang akan ditampilkan oleh material app
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Aplikasi Hello World"),
+          title: const Text('Anonymous Method'),
         ),
-        body: const Center(child: Text("Hello World")),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(message),
+              ElevatedButton(
+                  // onPressed: tombolDiTekan,
+                  //menggunakan anonymous jika hanya digunakan dalam satu tempat saja
+                  onPressed: () {
+                    setState(() {
+                      message = "Tombol sudah ditekan";
+                    });
+                  },
+                  child: const Text('Tekan Saya'))
+            ],
+          ),
+        ),
       ),
     );
   }
