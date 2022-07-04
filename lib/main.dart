047bf18eafdf023be8fd4674fc1a2ca89643cf22
krisnachy <cahyayuana@gmail.com>
2022-07-04 15:30:04 +0700

    latihan draggable

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,117 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Color color1 = Colors.red;
+  Color color2 = Colors.amber;
+  Color targetColor;
+  bool isAccepted = false;
+
+  @override
   Widget build(BuildContext context) {
-    //Material app adalah widget yg berisi data-data yg diperlukan oleh app yg menggunakan material design
     return MaterialApp(
-      //tampilan awal yang akan ditampilkan oleh material app
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Aplikasi Hello World"),
+          title: Text('Latihan Draggable'),
         ),
-        body: const Center(child: Text("Hello World")),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Draggable<Color>(
+                  data: color1,
+                  child: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      color: color1,
+                      shape: StadiumBorder(),
+                      elevation: 3,
+                    ),
+                  ),
+                  childWhenDragging: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      color: Colors.black38,
+                      shape: StadiumBorder(),
+                      elevation: 0,
+                    ),
+                  ),
+                  feedback: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      color: color1.withOpacity(0.7),
+                      shape: StadiumBorder(),
+                      elevation: 3,
+                    ),
+                  ),
+                ),
+                Draggable<Color>(
+                  data: color2,
+                  child: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      color: color2,
+                      shape: StadiumBorder(),
+                      elevation: 3,
+                    ),
+                  ),
+                  childWhenDragging: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      color: Colors.black38,
+                      shape: StadiumBorder(),
+                      elevation: 0,
+                    ),
+                  ),
+                  feedback: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      color: color2.withOpacity(0.7),
+                      shape: StadiumBorder(),
+                      elevation: 3,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            DragTarget<Color>(
+              onWillAccept: (value) => true,
+              onAccept: (value) {isAccepted = true; targetColor = value;},
+              builder: (context, candidates, rejected) {
+                return (isAccepted) ? SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: Material(
+                      color: targetColor,
+                      shape: StadiumBorder(),
+                    ),
+                  ) : SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: Material(
+                      color: Colors.black,
+                      shape: StadiumBorder(),
+                    ),
+                  );
+              },
+            )
+          ],
+        ),
       ),
     );
   }
