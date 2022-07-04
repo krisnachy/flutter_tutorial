3f580d3c5a177d1378c5c707bf8d7b291e867433
krisnachy <cahyayuana@gmail.com>
2022-07-04 13:54:16 +0700

    Contoh penggunaan listview

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,76 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  //Membuat list widget
+  List<Widget> widgets = [];
+  int counter = 1;
+
+  //membuat constructor
+  // _MyAppState() {
+  //   for (var i = 0; i < 15; i++) {
+  //     widgets.add(Text("Data ke-$i"));
+  //   }
+  // }
+
+  @override
   Widget build(BuildContext context) {
-    //Material app adalah widget yg berisi data-data yg diperlukan oleh app yg menggunakan material design
     return MaterialApp(
-      //tampilan awal yang akan ditampilkan oleh material app
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Aplikasi Hello World"),
+          title: Text('Latihan ListView'),
         ),
-        body: const Center(child: Text("Hello World")),
+        //Jika menggunakan kolom ketika data melebihi layar akan overflow sehingga harus menggunakan listview
+        // body: Column(
+        //   children: widgets,
+        // ),
+        body: ListView(
+          children: [
+            Row(
+              //sisa space akan dibagi rata jika menggunakan space aroun
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                // ElevatedButton(child: Text('Tambah Data'), onPressed: null,),
+                // ElevatedButton(child: Text('Hapus Data'), onPressed: null,)
+                ElevatedButton(
+                  child: Text('Tambah Data'),
+                  onPressed: () {
+                    setState(() {
+                      widgets.add(Text(
+                        'Data ke-$counter',
+                        style: TextStyle(fontSize: 20),
+                      ));
+                      counter++;
+                    });
+                  },
+                ),
+                ElevatedButton(
+                  child: Text('Hapus Data'),
+                  onPressed: () {
+                    setState(() {
+                      widgets.removeLast();
+                      counter--;
+                    });
+                  },
+                )
+              ],
+            ),
+            Column(
+                crossAxisAlignment: CrossAxisAlignment.start, children: widgets)
+          ],
+        ),
       ),
     );
   }
