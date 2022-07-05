1dd7fe76249c5ae6eaa75c0e231053e80a68c50f
krisnachy <cahyayuana@gmail.com>
2022-07-05 11:35:18 +0700

    Membuat tabbar

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,41 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     //Material app adalah widget yg berisi data-data yg diperlukan oleh app yg menggunakan material design
     return MaterialApp(
-      //tampilan awal yang akan ditampilkan oleh material app
-      home: Scaffold(
+        //tampilan awal yang akan ditampilkan oleh material app
+        home: DefaultTabController(
+      length: 4,
+      child: Scaffold(
         appBar: AppBar(
-          title: const Text("Aplikasi Hello World"),
+          title: Text("Latihan Tabbar"),
         ),
-        body: const Center(child: Text("Hello World")),
+        bottomNavigationBar: TabBar(
+          tabs: [
+            Tab(
+              icon: Icon(Icons.comment),
+              text: "Comments",
+            ),
+            Tab(
+              child: Image(
+                image: AssetImage("assets/cute_icon.png"),
+              ),
+            ),
+            Tab(
+              icon: Icon(Icons.computer),
+            ),
+            Tab(
+              text: "News",
+            )
+          ],
+        ),
+        body: TabBarView(
+          children: [
+            Center(child: Text("Tab 1"),),
+            Center(child: Text("Tab 2"),),
+            Center(child: Text("Tab 3"),),
+            Center(child: Text("Tab 4"),),
+          ],
+        ),
       ),
-    );
+    ));
   }
 }
