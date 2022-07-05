0f659bd16a1d20245adb8a6e507ae9e91e848f49
krisnachy <cahyayuana@gmail.com>
2022-07-05 10:49:39 +0700

    Membuat button sendiri

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ void main() {
   runApp(const MyApp());
 }
 
+//Inkwell untuk memmebrikan warna gradasi pada button tidak seperti elevated button
+
 class MyApp extends StatelessWidget {
   const MyApp({Key key}) : super(key: key);
 
@@ -14,9 +16,44 @@ class MyApp extends StatelessWidget {
       //tampilan awal yang akan ditampilkan oleh material app
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Aplikasi Hello World"),
+          title: const Text("Latihan Membuat Button"),
         ),
-        body: const Center(child: Text("Hello World")),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            RaisedButton(
+              onPressed: () {},
+              color: Colors.amber,
+              shape: const StadiumBorder(),
+              child: const Text("Raised Button"),
+            ),
+            Container(
+              width: 150,
+              height: 40,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  gradient: const LinearGradient(
+                      colors: [Colors.purple, Colors.pink],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter)),
+              child: Material(
+                //harus dikasih transparan karena material numpuk
+                color: Colors.transparent,
+                borderRadius: BorderRadius.circular(20),
+                child: InkWell(
+                  splashColor: Colors.amber,
+                  borderRadius: BorderRadius.circular(20),
+                  onTap: () {},
+                  child: const Text(
+                    "My Button",
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.w600),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
