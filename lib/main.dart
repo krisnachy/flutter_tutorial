3e86e719322ea0b376dd2d0be25c9543d7764882
krisnachy <cahyayuana@gmail.com>
2022-07-18 11:30:59 +0700

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_yt/cart.dart';
+import 'package:flutter_yt/money.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +17,99 @@ class MyApp extends StatelessWidget {
     //Material app adalah widget yg berisi data-data yg diperlukan oleh app yg menggunakan material design
     return MaterialApp(
       //tampilan awal yang akan ditampilkan oleh material app
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Aplikasi Hello World"),
+      home: MultiProvider(
+        providers: [
+          ChangeNotifierProvider<Money>(
+            builder: (context) => Money(),
+          ),
+          ChangeNotifierProvider<Cart>(
+            builder: (context) => Cart(),
+          ),
+        ],
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.purple,
+            title: const Text("Multi Provider"),
+          ),
+          floatingActionButton: Consumer<Money>(
+            builder: (context, money, _) => Consumer<Cart>(
+              builder: (context, cart, _) => FloatingActionButton(
+                onPressed: () {
+                  if (money.balance >= 500) {
+                    cart.quantity += 1;
+                    money.balance -= 500;
+                  }
+                },
+                child: Icon(Icons.add_shopping_cart),
+                backgroundColor: Colors.purple,
+              ),
+            ),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Balance"),
+                    Container(
+                      height: 30,
+                      width: 150,
+                      margin: EdgeInsets.all(5),
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          color: Colors.purple[100],
+                          border: Border.all(color: Colors.purple, width: 2)),
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: Consumer<Money>(
+                          builder: (context, money, _) => Text(
+                            money.balance.toString(),
+                            style: TextStyle(
+                                color: Colors.purple,
+                                fontWeight: FontWeight.w700),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  height: 30,
+                  margin: EdgeInsets.all(5),
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      border: Border.all(color: Colors.black, width: 2)),
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Consumer<Cart>(
+                      builder: (context, cart, _) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Orange (500) x " + cart.quantity.toString(),
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            (500 * cart.quantity).toString(),
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w500),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
-        body: const Center(child: Text("Hello World")),
       ),
     );
   }
