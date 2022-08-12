3637c83618c4b3f596805991802a19eab9dfccc1
krisnachy <cahyayuana@gmail.com>
2022-08-12 13:53:03 +0700

    first

--- lib/color_bloc.dart
+++ lib/color_bloc.dart
@@ -1,18 +1,46 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 
 enum ColorEvent {to_amber, to_light_blue}
 
-class ColorBloc extends Bloc<ColorEvent, Color> {
-  Color _color = Colors.amber;
+//diubah ke hydratedbloc
+// class ColorBloc extends Bloc<ColorEvent, Color> {
+class ColorBloc extends HydratedBloc<ColorEvent, Color> {
+  //ketika menggunakan bloc dart tidak perlu filed untuk menyimpan state
+  // Color _color = Colors.amber;
   
+  //tanda tanya 2kali untuk mengecek apakah null
   @override
-  Color get initialState => Colors.amber;
+  Color get initialState => super.initialState ?? Colors.amber;
   
   @override
   Stream<Color> mapEventToState(ColorEvent event) async* {
+    /*
+    TIDAK PERLU JIKA MENGGUNAKAN BLOC
     _color = (event == ColorEvent.to_amber) ? Colors.amber : Colors.lightBlue;
-    //yield adalah perintah memasukkan data ke dalam stream
-    yield _color;
+    yield adalah perintah memasukkan data ke dalam stream
+    yield _color; */
+    //selain mengembalikan stream akan mengupdate state
+    yield (event == ColorEvent.to_amber) ? Colors.amber : Colors.lightBlue;
+  }
+  
+  @override
+  Color fromJson(Map<String, dynamic> json) {
+    try {
+      return Color(json['color'] as int);
+    } catch (e) {
+      return null;
+    }
+  }
+  
+  @override
+  Map<String, int> toJson(Color state) {
+    try {
+      //.value untuk mengubah ke int
+      return {'color': state.value};
+    } catch (e) {
+      return null;
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_yt/color_bloc.dart';
+import 'package:bloc/bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 
-void main() {
+void main() async {
+  BlocSupervisor.delegate = await HydratedBlocDelegate.build();
   runApp(const MyApp());
 }
 
